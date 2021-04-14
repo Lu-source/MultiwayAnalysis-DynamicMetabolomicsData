afd23e9aa92a64302bec6017daa5bf1624575bcb
Lu-source <66491155+Lu-source@users.noreply.github.com>
2021-04-14 13:51:34 +0200

    Update CP_missing_example.m

--- Code_CP_Paralind/CP_missing_example.m
+++ Code_CP_Paralind/CP_missing_example.m
@@ -4,25 +4,24 @@
 % In addition, parts of the scripts may require the dataset object (https://eigenvector.com/software/dataset-object/), publically available.
 
 %% load data
-load LOS_beta001_alpha05.mat
+load GLM_beta002_PFKalpha05.mat
 Xorig = tensor(Y.data);
 
 %% add noise
-eta    = 0.1; %the level of noise
+eta    = 0.3; %the level of noise
 N      = tensor(randn(size(Xorig)));
 Xnoise = Xorig + eta*N/norm(N)*norm(Xorig);
 
 %% algorithmic options
-nb_starts = 10;
+nb_starts = 40;
 nm_comp   = 2;
-%optionsCP.factr=1e-10;
 optionsCP.maxIts      = 10000;
 optionsCP.maxTotalITs = 50000;
 optionsCP.printEvery  = 10000;
 Low{1} = -Inf*ones(size(Xorig,1),nm_comp);
 Low{2} = -Inf*ones(size(Xorig,2),nm_comp);
 Low{3} = zeros(size(Xorig,3),nm_comp);
-  
+
 %% CP model with missing data
 mis_perc = 0.2*ones(1,20); % missing percentage
 
@@ -31,28 +30,28 @@ for q = 1:length(mis_perc) % loop for 20 different missing data patterns
     W = create_missing_data_pattern(size(Xorig),mis_perc(q));
     data.Wall{q} = W; %store the missing position for each sample
     
+    
     % set missing value to be NaN
     Xmiss = Xnoise;
     Xmiss(find(W.data==0))= NaN;
     
-    % preprocess the data 
+    %  preprocess the data
     %  centering across the subjects mode
     XX   = Xmiss.data;
     temp = XX(:,:);
     temp_centered = temp - repmat(nanmean(temp),size(temp,1),1);
-    XX_centered   = reshape(temp_centered, size(XX));
-    X_centered    = tensor(XX_centered);
-    % scaling in the metabolites mode - using root mean square
+    X_centered   = reshape(temp_centered, size(XX));
+    %  scaling in the metabolites mode - using root mean square
     X = X_centered;
     for j = 1:size(X,2)
-        temp = squeeze(X.data(:,j,:));
+        temp = squeeze(X(:,j,:));
         rms  = sqrt(nanmean((temp(:).^2)));
         XX(:,j,:) = temp/rms;
     end
     Xpre = tensor(XX);
     Xpre(find(W.data==0))=0;
     
-    % Perform the CP model
+    %  Perform the CP model
     X = Xpre;
     goodness_X1 = strings(nb_starts,1);
     goodness_X  = zeros(nb_starts,1); %Stores ExitMsg, Fit, F(error for lbfgsb)
@@ -63,9 +62,9 @@ for q = 1:length(mis_perc) % loop for 20 different missing data patterns
     for i = 1:nb_starts
         if i==1
             [Fac_X{i}, ~, out_X{i}] = cp_wopt(X,W,nm_comp,'init','nvecs','lower',Low,'opt_option',optionsCP);
-        else            
-            [Fac_X{i}, ~, out_X{i}] = cp_wopt(X,W,nm_comp,'init','randn','lower',Low,'opt_option',optionsCP);         
-        end        
+        else
+            [Fac_X{i}, ~, out_X{i}] = cp_wopt(X,W,nm_comp,'init','randn','lower',Low,'opt_option',optionsCP);
+        end
         goodness_X1(i) = out_X{i}.ExitMsg;
         goodness_X(i)  = out_X{i}.OptOut.err(end,1);
     end
@@ -73,7 +72,6 @@ for q = 1:length(mis_perc) % loop for 20 different missing data patterns
     % the factor for each sample
     [ff, index] = sort(goodness_X(:),'ascend');
     Fac = Fac_X{index(1)};
-    %data.fit(q) = 100- (norm(tensor(full(Fac)-X))^2/norm(X)^2*100);
     data.exitMS{q} = goodness_X1(index(1));
     
     % To compute the TCS, we need the postprocessing
@@ -82,7 +80,7 @@ for q = 1:length(mis_perc) % loop for 20 different missing data patterns
     X      = X_centered;
     %%scaling back within the metabolites mode
     for j=1:size(X,2)
-        temp      = squeeze(X.data(:,j,:));
+        temp      = squeeze(X(:,j,:));
         temptilde = squeeze(Xtilde.data(:,j,:));
         rms       = sqrt(nanmean((temp(:).^2)));
         XX(:,j,:) = temptilde*rms;
@@ -100,7 +98,7 @@ for q = 1:length(mis_perc) % loop for 20 different missing data patterns
     data.TS(q)   = norm(tensor(times(W,Xpredic-Xnoise)))/norm(tensor(times(W,Xnoise)));
     data.Xhat{q} = Xpredic;
 end
- 
+
 
 %% plot predicted--actual
 data.X = Xorig;
@@ -113,7 +111,7 @@ for q = 1:length(data.Xhat)
     ylabel('predicted')
 end
 
-%% save the data
+% %% save the data
 % CP2_Miss20_data_noise_03=data;
 % save ('CP2_Miss20_data_noise_03','CP2_Miss20_data_noise_03')
 
