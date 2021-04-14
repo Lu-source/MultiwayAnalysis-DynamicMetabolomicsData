0bdba2ec8365ebc71ec00546dadf0810a2fd3e73
Lu-source <66491155+Lu-source@users.noreply.github.com>
2021-04-14 13:52:06 +0200

    Update TC.m

--- Code_CP_Paralind/TC.m
+++ Code_CP_Paralind/TC.m
@@ -1,11 +1,14 @@
 
 % This function is used to compute the Tucker congruency
+% X is a cell, 
+% z is a vector; 
+% z stores the TC values of the j-th (j=1,2...) and k-th (j+1,...) component
 
 function z=TC(X) 
-% X is a cell, TC means Tucker Congurancy
 
 m=length(X);
-for i=1:m(1)
+
+for i=1:m     %Loop in each mode
     s=size(X{i});
     for j=1:s(2)-1
         for k=(j+1):s(2)
@@ -14,5 +17,6 @@ for i=1:m(1)
     end
 end
 z=prod(y);
+
 
 
